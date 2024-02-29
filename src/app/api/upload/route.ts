8e674ec3225953ae52fd0ba026112c64841127ce
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2024-02-29 21:50:02 +0530

    feat: fixed the route.ts issue(deleted it kek)

--- src/app/api/upload/route.ts
+++
@@ -1,8 +0,0 @@
-// import { NextRequest, NextResponse } from "next/server"
-
-// export async function handler(req:NextRequest, res:NextResponse) {
-//     if(req.method === 'GET'){
-//       const message = "hello world :)"
-//       res.json({message})
-//     }
-//   }
