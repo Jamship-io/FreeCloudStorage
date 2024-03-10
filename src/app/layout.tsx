c1eeb421a6e87d835d3b74ba3f0554998bed44dd
arslan <arslanmudasir111@gmail.com>
2024-03-10 19:23:22 +0530

    bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,8 @@ import { Inter } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
 import SessionProvider from "./_components/providers/SessionProvider";
-import { getServerSession } from "next-auth";
+// import { getServerSession } from "next-auth";
+import { getSession } from "next-auth/react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,7 +23,7 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession()
+  const session = await getSession()
   return (
     <html lang="en">
       <body className={`font-sans ${inter.variable} flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white`}>
