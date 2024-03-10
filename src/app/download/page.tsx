a59efa2e855cfa22e63a63f108df91aa14fec875
arslan <arslanmudasir111@gmail.com>
2024-03-11 00:35:36 +0530

    bug fixes

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -1,10 +1,10 @@
 import { saveAs } from "file-saver";
-import Download from "../_components/download/Download";
+// import Download from "../_components/download/Download";
 
 function Downlaod() {
   return (
     <div className="min-w-screen min-h-screen bg-black text-white">
-      <Download></Download>
+      {/* <Download></Download> */}
     </div>  
 
   );
